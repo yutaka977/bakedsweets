bced88bf2e08fc1fe626c65128a69d7dd868edd6
yutaka977 <sb0119se@ed.ritsumei.ac.jp>
2025-02-23 19:07:56 +0900

    変更内容をメモ

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -5,3 +5,8 @@ export default class extends Controller {
     this.element.textContent = "Hello World!"
   }
 }
+document.addEventListener("DOMContentLoaded", function () {
+  setTimeout(() => {
+      document.querySelector(".slide").style.opacity = "1";
+  }, 3000); // 黒い画面が消えた後に最初の画像がフェードイン
+});
